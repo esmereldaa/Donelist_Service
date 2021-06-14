44c3d5e0e8c792d1e02896a515722df3e96ff19b
esmereldaa <fatulloh.luthfi@gmail.com>
2021-06-14 17:08:59 +0700

    Update

--- database/migrations/2021_06_14_1623662941_create_donelist_table.php
+++ database/migrations/2021_06_14_1623662941_create_donelist_table.php
@@ -11,7 +11,7 @@ class CreateDonelistTable extends Migration
         Schema::create('donelist', function (Blueprint $table) {
 
         $table->increments('list_id')->start_from(10);
-		$table->foreign('user_id')->references('user_id')->on('user')->onDelete('cascade');
+		$table->foreign('data_user_id',5);
 		$table->string('task_data')->nullable();
 		$table->datetime('date')->nullable();
 
